8ed8e2b94400a7b43307bad6c8026b4a9ad6e17b
Kethlyn <kethlynsaraa@gmail.com>
2022-06-15 02:00:24 -0300

    feat: like/deslike a post, change icon color

---
+++ src/components/Likes.js
@@ -0,0 +1,56 @@
+import axios from "axios";
+import { useState } from "react";
+import { FaRegHeart, FaHeart } from "react-icons/fa";
+import styled from "styled-components";
+
+function Likes() {
+    const [icon, setIcon] = useState(false);
+    async function likePost() {
+        try {
+            const {request} = await axios.post("http://localhost:8000/likes", { publicationId: 1 }) 
+            const {response} = request
+
+            console.log(response, request)
+
+            if (response === "likeSuccess") {
+                console.log("like")
+                setIcon(!icon)
+                
+            }
+
+            if (response === "deslikeSuccess") {
+                console.log("deslike")
+                setIcon(!icon)
+            }
+        } catch (e) {
+            console.log(e);
+        }
+    }
+
+    function getIcon() {
+        return icon ? (
+            <FaHeart style={{fill: "#AC0000"}} onClick={likePost}/>
+        )
+        :
+        (
+            <FaRegHeart style={{fill: "#FFFFFF"}} onClick={likePost}/>
+        )
+    }
+    return (
+        <Container style={{background: "#000"}} >         
+            {getIcon()}
+        </Container>
+    )
+}
+
+export default Likes;
+
+const Container = styled.div`
+    margin-left: 50px;
+    width: 50px;
+
+    svg {
+        width: 20px;
+        height: 18px;
+    }
+`;
