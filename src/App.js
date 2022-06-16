8a06567714ca16c7eee76aee100c3d2114cb4399
Offjhonjhon <jjvictorac@gmail.com>
2022-06-15 21:31:31 -0300

    feat: redirected to /hashtag/:hastag when click tag

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import {
+    BrowserRouter as Router,
+    Routes as Switch,
+    Route
+} from 'react-router-dom';
+
+
+import './assets/styles/reset.css';
+import './assets/styles/style.css';
+
+export default function App() {
+    return (
+        <Router>
+            <Switch>
+
+            </Switch>
+        </Router>
+    );
+}
---
+++ src/components/Hashtag.js
@@ -0,0 +1,34 @@
+
+import ReactHashtag from "react-hashtag";
+import styled from "styled-components";
+
+function Hashtag(props) {
+    return (
+        <HashtagContainer>
+            <ReactHashtag
+                renderHashtag={(hashtagValue) => (
+                    <StyledHashtag
+                        href={`/hashtag/${hashtagValue.replace("#", "")}`}
+                    >
+                        {hashtagValue}
+                    </StyledHashtag>
+                )}
+            >
+                {props.children}
+            </ReactHashtag>
+        </HashtagContainer>
+    );
+}
+
+export default Hashtag;
+
+const StyledHashtag = styled.a`
+    color: #ffffff;
+    font-weight: bold;
+    text-decoration: none;
+    display: inline;
+`;
+
+const HashtagContainer = styled.div`
+    
+`
