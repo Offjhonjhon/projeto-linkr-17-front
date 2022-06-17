1c30c0938ac61cd9697a8105c30db4608ff9d8c1
José Rodolpho <j.rodolpho29@hotmail.com>
2022-06-17 06:26:00 -0300

    fix: change app.js location

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,7 @@ import { useState } from 'react';
 
 import Likes from "./Likes.js";
 import Header from './Header.js';
-import SearchBar from './SearchBar.js';
+import SearchBar from './SearchBar';
 import SignIn from './SignIn.js';
 import SignUp from './SignUp.js';
 import StateContext from '../contexts/StateContext.js';
@@ -15,6 +15,7 @@ export default function App() {
         <StateContext.Provider value={{ visible, setVisible }}>
             <BrowserRouter>
                 <Header />
+                <SearchBar />
                 <Routes>
                     <Route path="/likes" element={<Likes />} />
                     <Route path="/sign-in" element={<SignIn />} />
